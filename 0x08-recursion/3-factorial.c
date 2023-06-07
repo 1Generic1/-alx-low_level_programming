1b58b902383f85611d8b16e653d8aeb9306aff9d
kelvin <kelvin4stunt@gmail.com>
2023-06-07 18:34:10 +0100

    recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial - function that returns the factorial of a given number
+ * @n: integer input
+ *
+ * Return: -1 to indicate an error, 1 if n = 0
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		/* Return -1 to indicate an error for negative numbers */
+		return (-1);
+	}
+	else if 
+	{
+		/* base case: factorial of 0 is 1 */
+		return(1);
+	}
+	else
+	{
+		/* Recursive case: multiply n with factorial of (n-1) */
+		return (n * factorial(n - 1);
+	}
+}
+		 
