fe978f94f965352c4e3c77c9c734d98eac3d5fed
kelvin <kelvin4stunt@gmail.com>
2023-06-07 21:19:11 +0100

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,6 +7,13 @@
  * Return: 1 if the string is a palindrome, 0 otherwise
  */
 
+/**
+ * _strlen_recursion - calculates the length of a string using recursion
+ * @s: the string
+ *
+ * Return: the length of the string
+ */
+
 int _strlen_recursion(char *s)
 {
         if (*s == '\0')
@@ -14,23 +21,6 @@ int _strlen_recursion(char *s)
 
         return (1 + _strlen_recursion(s + 1));
 }
-
-
-/* int is_palindrome(char *s)
-{
-	int len = _strlen_recursion(s);
-
-	if (len <= 1)
-	{
-		 this is the base case for the recursion 
-		return (1);
-	}
-	if (*s != *(s + len - 1))
-		return (0);
-	return (is_palindrome(s + 1) && is_palindrome(s + len - 2));
-} */
-
-
 /**
  * is_palindrome_recursive - recursive helper function to check if a string is a palindrome
  * @s: the string to check
@@ -41,13 +31,11 @@ int _strlen_recursion(char *s)
  */
 int is_palindrome_recursive(char *s, int start, int end)
 {
-    if (start >= end)
-        return 1;
-
-    if (s[start] != s[end])
-        return 0;
-
-    return is_palindrome_recursive(s, start + 1, end - 1);
+	if (start >= end)
+	return 1;
+	if (s[start] != s[end])
+	return 0;
+	return is_palindrome_recursive(s, start + 1, end - 1);
 }
 
 /**
@@ -58,10 +46,10 @@ int is_palindrome_recursive(char *s, int start, int end)
  */
 int is_palindrome(char *s)
 {
-    int len = _strlen_recursion(s);
+	int len = _strlen_recursion(s);
 
-    if (len <= 1)
-        return 1;
+	if (len <= 1)
+	return 1;
 
-    return is_palindrome_recursive(s, 0, len - 1);
+	return is_palindrome_recursive(s, 0, len - 1);
 }
