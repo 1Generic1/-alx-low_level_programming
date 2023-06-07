55c4f0d9bb80539f81d7bdcf1d845042f8c2837a
kelvin <kelvin4stunt@gmail.com>
2023-06-07 19:54:28 +0100

    recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,14 +14,11 @@ int wildcmp(char *s1, char *s2)
 	{
 		/* this is the base condition for this function */
 		return (1);
-	// If the current characters match or s2 has a wildcard '*', move to the next characters
     if (*s1 == *s2 || *s2 == '*')
         return wildcmp(s1 + 1, s2 + 1);
 
-    // If s2 has a wildcard '*', try to match s1 with the next character of s2 or skip the wildcard
     if (*s2 == '*')
         return wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2);
 
-    // If none of the above conditions are met, the strings are not identical
-    return 0;
+    return (0);
 }
