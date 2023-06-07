7165f8a97540c0c4efbde595a665310f274e19fe
kelvin <kelvin4stunt@gmail.com>
2023-06-07 23:12:13 +0100

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,11 +8,11 @@
  */
 int _sqrt_recursive(int n, int i)
 {
-       	if (i * i > n)
-	       	return -1;
+	if (i * i > n)
+		return -1;
 	if (i * i == n)
-	return i;
-	return _sqrt_recursive(n, i + 1);
+	return (i);
+	return (_sqrt_recursive(n, i + 1));
 }
 
 /**
