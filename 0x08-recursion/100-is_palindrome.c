9e068d27d8df93ad553d6aded3f73c4fcf15aff1
kelvin <kelvin4stunt@gmail.com>
2023-06-07 22:00:11 +0100

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,12 +9,11 @@
  * _strlen_recursion - calculates the length of a string using recursion
  * @s: the string
  *
- * Return: the length of the string
  */
 
 int _strlen_recursion(char *s)
 {
-       	if (*s == '\0')
+	if (*s == '\0')
 		return (0);
 	return (1 + _strlen_recursion(s + 1));
 }
