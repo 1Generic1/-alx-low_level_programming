aa8280e1f7a2000309282c1595fbf5aa72de11cb
kelvin <kelvin4stunt@gmail.com>
2023-06-07 21:41:29 +0100

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -16,13 +16,13 @@
 
 int _strlen_recursion(char *s)
 {
-        if (*s == '\0')
-                return (0);
+       	if (*s == '\0')
+		return (0);
 
-        return (1 + _strlen_recursion(s + 1));
+	return (1 + _strlen_recursion(s + 1));
 }
 /**
- * is_palindrome_recursive - recursive helper function to check if a string is a palindrome
+ * is_palindrome_recursive - recursive helper function
  * @s: the string to check
  * @start: the starting index
  * @end: the ending index
@@ -32,10 +32,10 @@ int _strlen_recursion(char *s)
 int is_palindrome_recursive(char *s, int start, int end)
 {
 	if (start >= end)
-	return 1;
+	return (1);
 	if (s[start] != s[end])
-	return 0;
-	return is_palindrome_recursive(s, start + 1, end - 1);
+	return (0);
+	return (is_palindrome_recursive(s, start + 1, end - 1));
 }
 
 /**
@@ -49,7 +49,7 @@ int is_palindrome(char *s)
 	int len = _strlen_recursion(s);
 
 	if (len <= 1)
-	return 1;
+	return (1);
 
-	return is_palindrome_recursive(s, 0, len - 1);
+	return (is_palindrome_recursive(s, 0, len - 1));
 }
