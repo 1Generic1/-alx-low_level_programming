c2c5938e9d0e3302936c189390e5ea84ba98751c
kelvin <kelvin4stunt@gmail.com>
2023-06-07 19:47:56 +0100

    recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,13 +14,18 @@ int wildcmp(char *s1, char *s2)
 	{
 		/* this is the base condition for this function */
 		return (1);
-	}
-	if (*s1 == *s2 || *s2 == '*')
-		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*')
+		if (*s2 == '*')
 	{
-		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1, s2 + 1))
+			return (1);
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+			return (1);
 	}
-	else
-		return (0);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	return (0);
 }
