91534bf2c35e5ad9c300a1a208925efcd79fd68f
kelvin <kelvin4stunt@gmail.com>
2023-06-09 10:02:24 +0100

    argc

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-
+/**
+ * main - Entry point of the program
+ * @argc: number of command line argument in the program
+ * @argv: actual command line argument to the program
+ *
+ * Return: always 0 on success
+ */
 
 int main(int argc, char *argv[]__attribute__((__unused__)))
 {
