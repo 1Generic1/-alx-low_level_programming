61fa40bfb2983df548d6821c8182d3e39e30d9ce
kelvin <kelvin4stunt@gmail.com>
2023-06-13 15:36:14 +0100

    malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+char *_strdup(char *str)
+{
+	size_t lenght = strlen(str);
+	char *duplicate;
+	
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	duplicate = (char *) malloc(lenght + 1) * (sizeof (char))
+		if (duplicate == NULL)
+		{
+			return (NULL);
+		}
+	copied_duplicate = strcpy(duplicate, str);
+		return (copied_duplicate);
+}
