ac94a36100dce7b16a88f7baa698dbd162028cb1
kelvin <kelvin4stunt@gmail.com>
2023-06-13 15:41:14 +0100

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,6 +2,15 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+
+/**
+ * _strdup - function returns a pointer to a new string
+ * @str: the string
+ *
+ * Return: a pointer to the string
+ */
+
+
 char *_strdup(char *str)
 {
 	size_t lenght = strlen(str);
@@ -11,7 +20,7 @@ char *_strdup(char *str)
 	{
 		return (NULL);
 	}
-	duplicate = (char *) malloc(lenght + 1) * (sizeof (char))
+	duplicate = (char *) malloc(lenght + 1) * (sizeof (char));
 		if (duplicate == NULL)
 		{
 			return (NULL);
