21281fec92915bd3aad10feeb4ad7ee83d031b0f
kelvin <kelvin4stunt@gmail.com>
2023-06-20 11:35:27 +0100

    struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,5 +15,5 @@ void print_dog(struct dog *d)
 		return;
 	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
 	printf("Age: %.1f\n", d->age);
-	printf("owner: %s\n", (d->owner != NULL) ? d->owner: "(nil)");
+	printf("owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 }
