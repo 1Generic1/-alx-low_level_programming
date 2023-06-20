8d4bd756fa320f17ed254f6747fd5c89e167779e
kelvin <kelvin4stunt@gmail.com>
2023-06-20 09:38:02 +0100

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * struct dog - structure containing description of a dog
+ * @name: pointer to the dog's name
+ * @age: the dog's age
+ * @owner: pointer to the owner's name or contact information
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
