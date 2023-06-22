0fba5d6cba2e476157a3af10f4ef337a76d65345
kelvin <kelvin4stunt@gmail.com>
2023-06-22 18:09:13 +0100

    function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	for (i= 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		number = va_arg(args, int);
 		printf("%d", number);
