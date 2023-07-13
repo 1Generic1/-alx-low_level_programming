25909bd342b99fc528d9fa52995f424927075144
generic <kelvin4stunt@gmail.com>
2023-07-12 23:29:01 -0700

    file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * @filename: Name of the text file to read
+ * @letters: Number of letters to read and print
+ *
+ * Return: The actual number of letters read and printed, or 0 on failure
+ */
+
+Prototype: ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytes_read, bytes_written;
+	char buffer[1024];
+	
+	if (filename == NULL)
+	return (0);
+	
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	bytes_read = read(fd, buffer, sizeof(buffer));
+	if (bytes_read == -1)
+	{
+		close(fd);
+		return (0);
+	}
+	bytes_written = write(fd, buffer, bytes_read);
+	if (bytes_written == -1 || (size_t)bytes_written != (size_t)bytes_read)
+	{
+		close(fd);
+		return (0);
+	}
+	close(fd);
+	return (bytes_written);
+}
