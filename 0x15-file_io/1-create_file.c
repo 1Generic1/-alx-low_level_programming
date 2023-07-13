b2bc54e36ad2ecf7432766f9cacbef52c3e8bfa9
generic <kelvin4stunt@gmail.com>
2023-07-12 22:31:20 -0700

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,6 +30,13 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	}
+	else
+	{
+		if (close(fd) == -1)
+		{
+			return (-1);
+			return (1);
+		}
 	close(fd);
 	return (1);
 }
