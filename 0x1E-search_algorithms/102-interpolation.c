4bec58b7f8f3c9fc5813ba613fd3caf97135cca3
kelvin <kelvin4stunt@gmail.com>
2023-12-15 00:18:39 +0100

    up

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
 
 /**
- * interpolation_search - Searches for a value in a sorted array using Interpolation Search
+ * interpolation_search - Searches for a value in a
+ * sorted array using Interpolation Search
  * @array: Pointer to the first element of the array
  * @size: Number of elements in the array
  * @value: Value to search for
  *
- * Return: The first index where the value is located, or -1 if not present or array is NULL
+ * Return: The first index where the value is located,
+ * or -1 if not present or array is NULL
  */
 int interpolation_search(int *array, size_t size, int value)
 {
 	size_t low = 0;
-	size_t high = size - 1;
+	size_t hi = size - 1;
 	size_t pos;
+	double f;
 
 	if (array == NULL || size == 0)
 		return (-1);
-	while (low <= high && value >= array[low] && value <= array[high])
+	while (low <= hi && value >= array[low] && value <= array[hi])
 	{
 		/* cal the probe position using interpolation formula */
-		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+		f = (((double)(hi - low) / (array[hi] - array[low])) * (value - array[low]));
+		pos = low + f;
 		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 		if (array[pos] == value)
 			return (pos);
 		if (array[pos] < value)
 			low = pos + 1;
 		else
-			high = pos - 1;
+			hi = pos - 1;
 	}
 	return (-1);
 }
